b49d7104befde9a18bcd8c5c9138f2677cf3d61d
ihebbennaceur <ihebbennaceur6@gmail.com>
2025-05-02 15:42:52 +0200

    ajoute de comp plantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component'; 
+import { PlantsComponent } from './pages/plants/plants.component';
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+   {path : 'plants', component: PlantsComponent},
 
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
---
+++ src/app/pages/plants/plants.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plants',
+  imports: [],
+  templateUrl: './plants.component.html',
+  styleUrl: './plants.component.css'
+})
+export class PlantsComponent {
+
+}
