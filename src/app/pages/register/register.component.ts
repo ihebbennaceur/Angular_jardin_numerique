8438d54bda7347e663e339b4984dc128c532c898
ihebbennaceur <ihebbennaceur6@gmail.com>
2025-05-02 17:31:24 +0200

    finish register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,31 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { RouterLink } from '@angular/router';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [NgIf, RouterLink,
+    FormsModule
+  ],
+  standalone: true,
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  // name: string = '';
+  // surname: string = '';
+  // email: string = '';
+  // password: string = '';
+  // confirmPassword: string = '';
+  user: User = new User();
+
+  onSubmit():void{
+    console.log('Form submitted!' , this.user);
+  }
 
 }
