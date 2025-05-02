4e5edc4f4cb6108c51ab7dda7682ed4b215c51c0
ihebbennaceur <ihebbennaceur6@gmail.com>
2025-05-02 15:23:16 +0200

    create comp login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component'; 
+import{ Router } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    NavbarComponent],
+            NavbarComponent,
+            LoginComponent,
+            RegisterComponent],
+  
+  
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
